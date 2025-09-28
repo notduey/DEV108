faaa01aef27da2d071ea69b1a0fe12cc357cb320
notduey <notduey@gmail.com>
2025-09-28 00:12:02 -0700

    add shebang line and comment explaining its purpose

--- chapter1/hello-world.py
+++ chapter1/hello-world.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3 
+
+#The comment above makes the script executable on Unix-like systems. Shebang line is the first line in a python program and it tells the OS what to use to run the program. While not necessary for windows and macOS, it's good practice to include it for cross-platform compatibility.
+
 print("hello world") #print
 
 x = 3 #x variable
