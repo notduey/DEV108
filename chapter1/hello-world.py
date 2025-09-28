b90675fd05ad7de7b461543e4a41e1195a24604c
notduey <notduey@gmail.com>
2025-09-28 00:14:21 -0700

    add indentation comment and its relevance to python syntax

--- chapter1/hello-world.py
+++ chapter1/hello-world.py
@@ -2,6 +2,8 @@
 
 #The comment above makes the script executable on Unix-like systems. Shebang line is the first line in a python program and it tells the OS what to use to run the program. While not necessary for windows and macOS, it's good practice to include it for cross-platform compatibility.
 
+#indentation is critically important in python. It indicates a block of code. Python uses indentation to define the scope of loops, functions, and classes.
+
 print("hello world") #print
 
 x = 3 #x variable
