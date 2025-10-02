20f325258bb13ca7a7b06242ffabe0d58b38537d
notduey <notduey@gmail.com>
2025-10-02 04:44:05 -0700

    fixed shebang on files and finished chapter 2 homework

---
+++ chapter2/Chapter2HW.py
@@ -0,0 +1,26 @@
+#!/usr/bin/evr python3
+"""
+This program illustrates the use of variables, input() function, and string methods
+"""
+
+birth_year = 2007
+print(birth_year)
+
+birth_year += 10
+print(birth_year)
+
+birth_year -= 100
+print(birth_year)
+
+birth_year *= 2
+print(birth_year)
+
+user_name = input("Enter your name:")
+name = user_name
+print(name)
+
+name = user_name.capitalize()
+print(name)
+
+name = user_name.strip()
+print(name)
--- chapter2/testscores.py
+++ chapter2/testscores.py
@@ -1,4 +1,4 @@
-#!usr/bin/env python3
+#!/usr/bin/env python3
 
 #This is a tutorial program that illustrates the use of 
 #the while and if statements
