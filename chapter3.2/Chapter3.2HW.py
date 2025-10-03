be22c9b5f1b076b5ddbd540020490cb2d951c3a2
notduey <notduey@gmail.com>
2025-10-03 15:10:18 -0700

    finished Chapter3.2 homework file

--- chapter3.2/Chapter3.2HW.py
+++ chapter3.2/Chapter3.2HW.py
@@ -3,3 +3,8 @@
 This program illustrates the use of the for/while loop and prints
 all even numbers from 0-100 inclusively
 """
+i = 0
+for i in range(0, 101):
+    if i % 2 == 0:
+        print(i)
+        i += 2
