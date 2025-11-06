7591e4dbd73b4050d44fcff300cb0d2ab84b9d2a
notduey <notduey@gmail.com>
2025-11-05 18:24:49 -0800

    added chapter 5 homework files to modify

--- chapter7_8/chapter7_8_hw.py
+++ chapter7_8/chapter7_8_hw.py
@@ -1,4 +1,20 @@
-#!usr/bin/env python3
+#!/usr/bin/env python3
 """
-This program
+This program calls function from hw_05_module and simulates
+a user going to a fast food restaurant and ordering food
 """
+import hw_05_module as hw
+
+#show menu
+hw.show_food_menu()
+food_choice = hw.get_user_choice()
+
+hw.show_drink_menu()
+drink_choice = hw.get_user_choice()
+
+hw.show_dessert_menu()
+dessert_choice = hw.get_user_choice()
+
+#calculate total and print result
+total_price = hw.get_total_price(food_choice, drink_choice, dessert_choice)
+hw.print_total_price(total_price)
